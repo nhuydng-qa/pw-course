7074e04a1475f6ae7cfc04c0e98fdd5c0f7c4ff5
ydang <dangvannhuy1106@gmail.com>
2026-05-26 00:18:06 +0700

    update

--- tests/lesson-03/ex2.js
+++ tests/lesson-03/ex2.js
@@ -14,8 +14,9 @@ if (height > 100 && height < 200) {
     let idealWeight = (height - 100) * 9 / 10;
     let maxWeight = height - 100;
     let minWeight = (height - 100) * 8 / 10;
+
+    console.log(`Cân nặng lý tưởng: ${idealWeight}; Cân nặng tối đa: ${maxWeight}; Cân nặng tối thiểu: ${minWeight}`);
 } else {
     console.log("Chiều cao không hợp lệ!")
 }
 
-console.log(`Cân nặng lý tưởng: ${idealWeight}; Cân nặng tối đa: ${maxWeight}; Cân nặng tối thiểu: ${minWeight}`);
